a50ffeb431fced4e2c6c6213fe6f4f32e1427053
Jonathan Ming <jming@pkcsecurity.com>
2019-05-24 15:14:10 -0700

    Fix heroku deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const logger = require("morgan");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
