15d0b65143bca37e1ca57700d3208fb4823cef29
Jonathan Ming <jming@pkcsecurity.com>
2019-05-24 15:20:16 -0700

    Remove extra logging

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const logger = require("morgan");
 const port = process.env.PORT || 3000;
-
-app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
