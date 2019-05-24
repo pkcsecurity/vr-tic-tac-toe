77f5c127b4102f2d01d72d99a7868391e3f94596
Jonathan Ming <jming@pkcsecurity.com>
2019-05-24 15:11:44 -0700

    No more react, just plain express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const logger = require("morgan");
+const port = 3000;
+
+app.use(logger("dev"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
